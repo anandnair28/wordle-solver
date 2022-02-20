6b45c705962f86d642229d80d03a21a591da8cd6
Anand Nair <nairanand2812@gmail.com>
2022-02-20 19:52:47 +0530

    Data Cleaning Done Properly and Files Modified and Renamed

--- cleandata.py
+++ cleandata.py
@@ -5,11 +5,12 @@ n_char = 5
 input_file = open('data/words_alpha.txt', 'r')
 output_file = open('data/filtered_words.txt', 'w')
 
-count = 0
-
 for line in input_file:
     word = line.strip()
+    if not word.isalpha():
+        continue
     if len(word) == 5:
+        word = word.upper()
         output_file.write(word + '\n')
 
 input_file.close()
